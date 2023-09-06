62d0349b86d366922656256c98708e9d2bc10a7e
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-06 13:37:05 +0530

    Fix rubocop error

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
 class Solver
-    def factorial(number)
-        if number.negative?
-            raise ArgumentError, "Factorial of negative number is undefined"
-        elsif number.zero?
-            1
-        else
-            number * factorial(number - 1)
-        end
+  def factorial(number)
+    if number.negative?
+      raise ArgumentError, 'Factorial of negative number is undefined'
+    elsif number.zero?
+      1
+    else
+      number * factorial(number - 1)
     end
+  end
 
-    def fizz_buzz(number)
-        if number % 3 == 0 && number % 5 == 0
-            "fizzbuzz"
-        elsif number % 3 == 0
-            "fizz"
-        elsif number % 5 == 0
-            "buzz"
-        else
-            number
-        end
+  def fizz_buzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number
     end
+  end
 
-    def reverse_string(string)
-        string.reverse
-    end
+  def reverse_string(string)
+    string.reverse
+  end
 end
 
 solve = Solver.new
@@ -32,4 +32,4 @@ puts solve.fizz_buzz(6)
 puts solve.fizz_buzz(10)
 puts solve.fizz_buzz(15)
 puts solve.fizz_buzz(7)
-puts solve.reverse_string("hello")
+puts solve.reverse_string('hello')
