ed2ed5676ccb4bde44438d452e5c2e4e9b706d58
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-06 13:27:52 +0530

    Add tests for fizzbuzz

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -17,4 +17,19 @@ describe Solver do
             expect{solver.factorial(-1)}.to raise_error(ArgumentError)
         end
     end
+
+    describe 'fizzbuzz testing' do
+      it 'When N is divisible by 3, return "fizz"' do
+          expect(solver.fizz_buzz(6)).to eq("fizz")
+      end
+      it 'When N is divisible by 5, return "buzz"' do
+          expect(solver.fizz_buzz(10)).to eq("buzz")
+      end
+      it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
+          expect(solver.fizz_buzz(15)).to eq("fizzbuzz")
+      end
+      it 'when N is not divisible by 3 or 5, return N' do
+          expect(solver.fizz_buzz(7)).to eq(7)
+      end
+    end
 end
