37684b8aba2d45d73149463c8b4a2f284e820c39
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-06 13:30:36 +0530

    Add method for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -8,4 +8,23 @@ class Solver
             number * factorial(number - 1)
         end
     end
+
+    def fizz_buzz(number)
+        if number % 3 == 0 && number % 5 == 0
+            "fizzbuzz"
+        elsif number % 3 == 0
+            "fizz"
+        elsif number % 5 == 0
+            "buzz"
+        else
+            number
+        end
+    end
 end
+
+solve = Solver.new
+puts solve.factorial(5)
+puts solve.fizz_buzz(6)
+puts solve.fizz_buzz(10)
+puts solve.fizz_buzz(15)
+puts solve.fizz_buzz(7)
